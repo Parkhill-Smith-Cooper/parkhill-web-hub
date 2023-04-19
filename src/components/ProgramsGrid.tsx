0e69dc2d38081402790f4dc38aeb5d1cbef32989
Marcelo Romero <mromero@parkhill.com>
2023-04-19 12:24:18 -0500

    Refactor ProgramGrid

--- src/components/ProgramsGrid.tsx
+++ src/components/ProgramsGrid.tsx
@@ -14,27 +14,26 @@ const ProgramsGrid = ({ programQuery }: Props) => {
   const { data, error, isLoading } = usePrograms(programQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  if (error) return <Text>{error}</Text>;
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        padding="10px"
-        spacing={6}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
-            <ProgramCardContainer key={skeleton}>
-              <ProgramCardSkeleton />
-            </ProgramCardContainer>
-          ))}
-        {data.map((program) => (
-          <ProgramCardContainer key={program.id}>
-            <ProgramCard program={program} />
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => (
+          <ProgramCardContainer key={skeleton}>
+            <ProgramCardSkeleton />
           </ProgramCardContainer>
         ))}
-      </SimpleGrid>
-    </>
+      {data.map((program) => (
+        <ProgramCardContainer key={program.id}>
+          <ProgramCard program={program} />
+        </ProgramCardContainer>
+      ))}
+    </SimpleGrid>
   );
 };
 
