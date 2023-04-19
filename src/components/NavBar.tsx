fbfb00900acca1582b8b3552b9d2de62d0e3e4d3
Marcelo Romero <mromero@parkhill.com>
2023-04-19 09:25:10 -0500

    Build search input

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo_parkhill.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent={"space-between"} padding="10px">
+    <HStack padding="10px">
       <Image width="125px" objectFit="contain" src={logo} alt="Parkhill Logo" />
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
